c7b7f5156b3b0acbc9486dbb8241c6162cbf59c1
U-WIN2K18\ex111 <ex111@RP21SYS32.win2k18.local>
2023-12-19 10:15:38 +0530

    search main code commit

--- src/main/java/com/abc/App.java
+++ src/main/java/com/abc/App.java
@@ -1,13 +1,28 @@
+
 package com.abc;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int l[]={1,8,5,3};
+	int first=0,last=l.length-1;
+	int a[]=sort.sorting(l);
+	int key=5;
+   	int mid = (first + last)/2;  
+   	while( first <= last ){  
+      	if ( a[mid] < key ){  
+        first = mid + 1;    
+      }else if ( a[mid] == key ){  
+        System.out.println("Element is found at index: " + mid);  
+        break;  
+      }else{  
+         last = mid - 1;  
+      }  
+      mid = (first + last)/2;  
+   }  
+   if ( first > last ){  
+      System.out.println("Element is not found!");  
+   }
     }
 }
