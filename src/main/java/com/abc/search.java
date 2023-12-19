19e6debc8f07214803662d7eac96e04b7151e3a4
vijay-popuri <111995978+vijay-popuri@users.noreply.github.com>
2023-12-19 10:24:46 +0530

    Update search.java

--- src/main/java/com/abc/search.java
+++ src/main/java/com/abc/search.java
@@ -2,24 +2,7 @@ package com.abc;
 
 public class search {
  public static void main(String[] arg){
-       int l[]={1,8,5,3};
-int first=0,last=l.length-1;
-int a[]=sort.sorting(l);
-int key=5;
-   int mid = (first + last)/2;  
-   while( first <= last ){  
-      if ( a[mid] < key ){  
-        first = mid + 1;    
-      }else if ( a[mid] == key ){  
-        System.out.println("Element is found at index: " + mid);  
-        break;  
-      }else{  
-         last = mid - 1;  
-      }  
-      mid = (first + last)/2;  
-   }  
-   if ( first > last ){  
-      System.out.println("Element is not found!");  
+     System.out.println("hello world");
    }
 }
 }
